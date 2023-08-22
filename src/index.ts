f44d711b7cdbc3d9bbceb8c0b98ae4b9ce6b452a
veranikabarel <veranikabarel@gmail.com>
2023-08-22 21:56:33 +0200

    refactor generating function

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,12 @@
 import { quotes } from '../data/quotes';
 
-function getRandomIndex(min: number, max: number) {
+const getRandomIndex = (min: number, max: number) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1) + min);
-}
+};
 
-export const generateDunderMifflinIpsum = (p: number): string => {
-  const generatedText = [] as string[];
-  for (let i = 0; i < p; i++) {
-    generatedText.push(quotes[getRandomIndex(0, quotes.length)].quote);
-  }
-  return generatedText.join('\n');
+export const generateDunderMifflinIpsum = () => {
+  const quoteWithAuthor = quotes[getRandomIndex(0, quotes.length)];
+  return `'${quoteWithAuthor.quote}' - ${quoteWithAuthor.character}`;
 };
