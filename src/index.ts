340271130d1ba8507a02a5986293d87e4a57ce4b
veranikabarel <veranikabarel@gmail.com>
2023-08-22 18:46:01 +0200

    add function for generating random quotes

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-export const Greeter = (name: string) => `Hello ${name}`;
+import { quotes } from '../data/quotes';
+
+function getRandomIndex(min: number, max: number) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+export const generateDunderMifflinIpsum = (p: number): string => {
+  const generatedText = [] as string[];
+  for (let i = 0; i < p; i++) {
+    generatedText.push(quotes[getRandomIndex(0, quotes.length)].quote);
+  }
+  return generatedText.join('\n');
+};
